a8b3d9c9ec6b05baaecea10919cdc33576dcc988
gk-svg <gk32239@gmail.com>
2020-04-21 22:13:31 +0530

    added mycart/templates addede to TEMPLATE  DIRT

--- myCart/settings.py
+++ myCart/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'myCart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': ['myCart/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
