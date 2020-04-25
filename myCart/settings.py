374bc1f9db90e79499a0a2f9e66c54bdfcae0ef6
gk-svg <gk32239@gmail.com>
2020-04-25 12:54:57 +0530

    blog media files and changes due to blog

--- myCart/settings.py
+++ myCart/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+
 
 ]
 
