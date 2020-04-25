7a4a262a709edf438a1dc6cf2718bc6424f55e01
gk-svg <gk32239@gmail.com>
2020-04-25 12:43:32 +0530

    index function and blogpost function

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Blogpost
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'blog/home.html')
+    blogs = Blogpost.objects.all()
+    params = {'blogs': blogs}
+    return render(request, 'blog/index.html', params)
+
+
+def postView(request,id):
+    post = Blogpost.objects.filter(post_id = id)[0]
+    return render(request, 'blog/blogpost.html',{'post':post})
