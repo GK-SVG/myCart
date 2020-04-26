becc72efa18d428bf786e58e8e81442c6c979693
gk-svg <gk32239@gmail.com>
2020-04-26 17:19:42 +0530

    post_id accepted by url blogpost/<int:id>

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="blogHome"),
-    path("blogpost/<int:id>",views.postView, name="PostView"),
+    path("blogpost/<int:id>/",views.postView, name="PostView"),
 ]
